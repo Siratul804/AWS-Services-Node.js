49cc4d1f15d9564bbeb3119258fd33470436b14e
Siratul Islam <islamsiratul@gmail.com>
2025-04-03 23:42:27 +0600

    feat: added new s3 store

--- s3/index.js
+++ s3/index.js
@@ -1,4 +1,10 @@
-const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+  ListObjectsV2Command,
+  DeleteObjectCommand,
+} = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 const s3Client = new S3Client({
@@ -20,11 +26,48 @@ async function getObjectUrl(key) {
   return url;
 }
 
+async function putObject(filename, contentType) {
+  const command = new PutObjectCommand({
+    Bucket: "s3-bucket-name",
+    Key: "name of that file",
+    contentType: contentType,
+  });
+
+  const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
+
+  return url;
+}
+
+async function listObject() {
+  const command = new ListObjectsV2Command({
+    Bucket: "s3-bucket-name",
+    Key: "/",
+  });
+
+  const result = await s3Client.send(command);
+
+  console.log(result);
+}
+
 async function init() {
-  console.log(
-    "url for image/video/file",
-    await getObjectUrl("name of that file")
-  );
+  //   console.log(
+  //     "url for getting image/video/file",
+  //     await getObjectUrl("name of that file")
+  //   );
+
+  //   console.log(
+  //     "url for uploading",
+  //     await putObject(`image-${Date.now}.jpeg`, "image/jpeg")
+  //   );
+
+  //   await listObject();
+
+  const cmd = new DeleteObjectCommand({
+    Bucket: "s3-bucket-name",
+    Key: "name of that file",
+  });
+
+  await s3Client.send(cmd);
 }
 
 init();
